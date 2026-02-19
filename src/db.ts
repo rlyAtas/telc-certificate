d2a2de32a26a7650fe7d6b9b1f7e27a46be1a55a
Stanislav Shevchenko <stas.s.shevchenko@gmail.com>
2026-02-19 11:30:05 +0100

    fix prisma

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '@prisma/client';
-
+import { PrismaClient } from './generated/prisma/client.js';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
