9c623f06693203a5c886051d6129b3b96bce49d6
Stanislav Shevchenko <stas.s.shevchenko@gmail.com>
2026-02-21 11:28:22 +0100

    default value in dev mode

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,24 @@ import { Router } from 'express';
 
 export const routerIndex = Router();
 
+const isDev = process.env.NODE_ENV === 'development';
+
 routerIndex.get('/', (_req, res) => {
+
+  let values = {};
+
+  if (isDev) {
+    values = {
+      userNumber: '0382586',
+      birthDate: '1974-08-22',
+      examDate: '2026-02-09',
+      email: 'stas.s.shevchenko@gmail.com',
+    }
+  }
+
   res.render('index', {
     errors: {},
-    values: {},
+    values,
   });
 });
 
